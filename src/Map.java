4d486f29084ff3092d03b537b2b042e6544ffa8e
TakamichiNakamura <e155708@ie.u-ryukyu.ac.jp>
2016-12-13 11:59:52 +0900

    String=>intに変換中

--- src/Map.java
+++ src/Map.java
@@ -9,27 +9,50 @@ import java.io.File;
 
 
 public class Map {
+    int start_x,start_y;
+    int mapsize[];
+    Char map[][];               //マップ(char型二次元配列)
+    String goal;                //ゴール位置
+
+
 
     public static void main(String args[]) {
         try {
+
             File file = new File("map.txt");
             Scanner scan = new Scanner(file);
             scan.useDelimiter("¥¥r¥¥n");
-
-
             int line = 1;
-
-
             String str = scan.next();
             //System.out.println(str);
 
+            String[] str1 = str.split("\n");
+            String[] Start = str1[0].split(" ");
+            String[] Size = str1[1].split(" ");
+
+            start_x = Integer.perseInt(Start[0]);
+            //start_y = (int)Start[1];
+
+
+            //mapSize
+
+            //char start_x = str.charAt(1);
+            //int X = Integer.parseInt(""+start_x);
+            //start_y = str.charAt(1);
+            System.out.println(start_x);
+
+            /*scan.useDelimiter("¥¥r¥¥n");
+
+
+
+            char mapdata[][]= new char[][100];
+
             int i = 0;
-            char mapdata[];
-                while(i<100){
-                    mapdata[i] = str.CharAt(i);
-                    System.out.println(mapdata[i]);
-                    i++;
-                    if(i==20)break;
+            while(i<100){
+                mapdata[i] = str.charAt(i);
+                System.out.println(mapdata[i]);
+                i++;
+                if(i==20)break;
             }
 
 
@@ -47,7 +70,7 @@ public class Map {
                 line++;
             }*/
 
-            System.out.println(line);
+            //System.out.println(line);
 
 
         } catch (FileNotFoundException e) {
