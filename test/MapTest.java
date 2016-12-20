d65e8cb4b54493a09ba1200b4f3c95969211c021
takashift <e145771@ie.u-ryukyu.ac.jp>
2016-12-20 10:23:32 +0900

    pullのためにMerge

---
+++ test/MapTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Cokoten on 2016/12/06.
+ */
+public class MapTest {
+    public static void main(String args[]){
+       Map map = new Map("map.txt");
+    }
+}
