b7cc53269d874ac0b7320e92fcc068b3c7ec99fd
Ryo Matsumoto <e165738@ie.u-ryukyu.ac.jp>
2016-12-20 11:06:06 +0900

    Playerクラスを仮実装
    それに伴い、仕様書になかったこととして
    ●フィールド変数をプライベートに（カプセル化）
      → それに伴いget, set系も実装。
    ●placeは一次元配列にした。
    
    ●playerが移動する時の、「壁かどうかの判定」は、
        Mapとも関連してくるので、Mapが完成してから
        実装する。

--- src/ExMain.java
+++ src/ExMain.java
@@ -1,25 +1,31 @@
 import searching.Map;
+import searching.Player;
 
 public class ExMain{
-    Map map = new Map();
-    System.out.println(map.val);    //迷路インスタンスの生成
+    public main(String[] args) {
+        Map map = new Map();
+        int[] place = {map.start_x, map.start_y};
+        Player player = new Player(place);
 
-    Player player = new Player();
-    System.out.println(player.val); //プレイヤーインスタンス
 
-    map.printMap(map[][],plyer.place);
+        System.out.println(map.val);    //迷路インスタンスの生成
 
-    plyer.moveLeft();               //移動
+        Player player = new Player();
+        System.out.println(player.val); //プレイヤーインスタンス
 
-    map.printMap(map[][],plyer.place);
+        map.printMap(map, plyer.getplace());
 
-    plyer.moveDown();
+        plyer.moveLeft();               //移動
 
-    map.printMap(map[][],plyer.place);
+        map.printMap(map[][],plyer.place);
 
-    plyer.moveRight();              //右に移動（できない）
+        plyer.moveDown();
 
-    map.printMap(map[][],plyer.place);  //位置が変わっていないことの確認
+        map.printMap(map[][],plyer.place);
 
+        plyer.moveRight();              //右に移動（できない）
+
+        map.printMap(map[][],plyer.place);  //位置が変わっていないことの確認
+    }
 
 }
---
+++ src/searching/Player.java
@@ -0,0 +1,42 @@
+package searching;
+
+/**
+ * Created by M.R on 2016/12/20.
+ */
+public class Player {
+    private int[] place;   //プレイヤーの位置。[横, 縦](原点左上端)
+    private int count;   //歩数
+
+    public Player(int[] place){
+        this.place = place;
+        this.count = 0;
+    }
+
+    //　壁にぶつかったら止まる
+    public void moveUp(){     // 上に一歩移動するメソッド
+        this.place[1]--;
+        count++;
+    }
+    public void moveDown(){   // 下に一歩移動するメソッド
+        this.place[1]++;
+        count++;
+    }
+    public void moveRight(){  // 右に一歩移動するメソッド
+        this.place[0]++;
+        count++;
+    }
+
+    public void moveLeft(){   // 左に一歩移動するメソッド
+        count++;
+        this.place[0]--;
+    }
+
+    public int[] getPlace(){
+        return place;
+    }
+    public int getCount(){
+        return count;
+    }
+
+
+}
